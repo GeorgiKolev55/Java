a5280dad72292cb7bf9a269ab60f66b38157e574
GeorgiKolev55 <gkolev55@abv.bg>
2020-11-09 21:31:56 +0200

    exercise

--- PhraseValidator/src/Main/Validator.java
+++ PhraseValidator/src/Main/Validator.java
@@ -10,11 +10,12 @@ public class Validator {
         if (expression==null){
             throw new IllegalArgumentException("Input is Null");
         }
-        Pattern firstPattern = Pattern.compile("(A.*a|B.*b|C.*c)|(A.*aB.*b)|(C.*cA.*aB.*b)|(C.*cB.*bA.*a)|(A.*aB.*bC.*c)|(B.*bC.*cA.*a)|(B.*bA.*aC.*c)|(B.*bC.*c)|(C.*cA.*a)|(C.*cB.*b)|(B.*bA.*a)");
-        Pattern secondPattern = Pattern.compile("[AaBbCc]+");
+        Pattern firstPattern = Pattern.compile("(A.*a|B.*b|C.*c)|(C.*cA.*aB.*b)|(C.*cB.*bA.*a)|(A.*aB.*bC.*c)|(B.*bC.*cA.*a)|(B.*bA.*aC.*c)|(B.*bC.*c)|(C.*cA.*a)|(C.*cB.*b)|(B.*bA.*a)|(A.*aC.*c)|(A.*aB.*b)");
+        Pattern secondPattern =  Pattern.compile("[AaBbCc]+");
 
         Matcher matcherOne = firstPattern.matcher(expression);
         Matcher matcherTwo = secondPattern.matcher(expression);
+
 
         if (matcherOne.matches()&&matcherTwo.matches()){
            return true;
